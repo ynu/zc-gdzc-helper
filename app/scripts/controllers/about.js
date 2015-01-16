660e40de714d4a156926af6cd035b56ba6156b68
na57 <na57@qq.com>
2015-01-16 15:38:49 +0800

    初始化

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name zcGdzcHelperApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the zcGdzcHelperApp
+ */
+angular.module('zcGdzcHelperApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name zcGdzcHelperApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the zcGdzcHelperApp
+ */
+angular.module('zcGdzcHelperApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
